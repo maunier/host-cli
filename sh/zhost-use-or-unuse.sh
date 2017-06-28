1c615c7770b915931a95f1446d73d53f2e5ee5ff
赵瑞 <zhaorui@zhaoruideMacBook-Pro.local>
2017-06-28 14:24:01 +0800

    加上了zhost un功能

---
+++ sh/zhost-use-or-unuse.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+lines=$1
+isUse=$2
+hostFile='/etc/hosts'
+
+for line in ${lines[@]}
+do
+  if [ $isUse = 'true' ]; then
+    echo istrue:$isUse
+    sudo sed -i '' "${line}s/^#/ /" $hostFile
+  else
+    echo isfalse:$isUse
+    sudo sed -i '' "${line}s/^/#/" $hostFile
+  fi
+done
+
+cat -n $hostFile;
