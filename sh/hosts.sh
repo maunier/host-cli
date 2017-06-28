e47734e2dd22936d53268759bc8c4584c5e5f340
赵瑞 <zhaorui@zhaoruideMacBook-Pro.local>
2017-06-28 11:16:49 +0800

    add 后输出修改后的host文件

--- sh/hosts.sh
+++ sh/hosts.sh
@@ -1,9 +1,17 @@
 #!/bin/zsh
 
-host=$1
-ip=$2
+host=$2
+ip=$1
+hostFile='/etc/hosts'
+
+if [ ! -n $host -o ! -n $ip ]
+then
+  exit 2
+fi
 
 sudo sed -i "" "
-1i\\
-$host $ip
-" '/etc/hosts'
+  1i\\
+  $ip $host
+  " $hostFile;
+
+cat -n $hostFile;
