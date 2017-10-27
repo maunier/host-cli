5afe2d465d5657f1630c828176cab9bec1bd0994
zhaorui <zhaorui140280@163.com>
2017-10-27 19:10:46 +0800

    fix zhost add 后面的参数可以随意添加，不再限制

--- sh/zhost-add.sh
+++ sh/zhost-add.sh
@@ -1,21 +1,14 @@
 #!/bin/zsh
 
-ip=$1
-host=$2
-comment=$3
+IFS=,
+content=$1
 hostFile='/etc/hosts'
 
-if [ "$host" = 'undefined' -o "$ip" = 'undefined' ]; then
-  echo need ip and host
-  exit 2
-fi
+for param in ${content[@]}
+do
+  addStr="$addStr $param"
+done
 
-addStr="$ip $host"
-if [ "$comment" != 'undefined' ]; then
-  addStr="$addStr# $comment"
-fi
-
-# $ip $host # $comment
 sudo sed -i '.bac' "
   \$a\\
   $addStr
